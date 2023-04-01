69096f65f89256eea23ca4ea4fbab70aec23a267
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2023-04-01 12:24:34 +0100

    make tests pass

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,20 +1,23 @@
 package store
 
 func New() *Store {
-	return &Store{}
+	return &Store{
+		data: make(map[string]string),
+	}
 }
 
 // Store is a simple key-value store.
 type Store struct {
+	data map[string]string
 }
 
 // Set stores the given value under the given key.
 func (s *Store) Set(key, value string) {
-
+	s.data[key] = value
 }
 
 // Get returns the value stored under the given key.
 // If the key does not exist, it returns an error.
 func (s *Store) Get(key string) (string, error) {
-	return "", nil
+	return s.data[key], nil
 }
