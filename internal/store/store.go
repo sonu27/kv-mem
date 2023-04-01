bff540010bfa82caf34e4d40ee2fda798df334fa
Amarjeet Singh Rai <sonu27@users.noreply.github.com>
2023-04-01 12:53:19 +0100

    implement max key and value length

--- internal/store/store.go
+++ internal/store/store.go
@@ -5,22 +5,39 @@ import (
 	"fmt"
 )
 
-var ErrNotFound = errors.New("not found")
+var (
+	ErrNotFound    = errors.New("not found")
+	ErrorMaxKeyLen = errors.New("max key length exceeded")
+	ErrorMaxValLen = errors.New("max val length exceeded")
+)
 
-func New() *Store {
+func New(maxKeyLen, maxValLen int) *Store {
 	return &Store{
-		data: make(map[string]string),
+		maxKeyLen: maxKeyLen,
+		maxValLen: maxValLen,
+		data:      make(map[string]string),
 	}
 }
 
 // Store is a simple key-value store.
 type Store struct {
-	data map[string]string
+	maxKeyLen int
+	maxValLen int
+	data      map[string]string
 }
 
 // Set stores the given value under the given key.
-func (s *Store) Set(key, value string) {
+func (s *Store) Set(key, value string) error {
+	if len(key) > s.maxKeyLen {
+		return fmt.Errorf("%w: key: %s", ErrorMaxKeyLen, key)
+	}
+
+	if len(value) > s.maxValLen {
+		return fmt.Errorf("%w: value: %s", ErrorMaxValLen, value)
+	}
+
 	s.data[key] = value
+	return nil
 }
 
 // Get returns the value stored under the given key.
--- internal/store/store_test.go
+++ internal/store/store_test.go
@@ -6,9 +6,20 @@ import (
 	"testing"
 )
 
+func TestSet_max_key_len_exceeded(t *testing.T) {
+	s := store.New(5, 10)
+	err := s.Set("123456", "bar")
+	assert.ErrorIs(t, err, store.ErrorMaxKeyLen)
+}
+
+func TestSet_max_val_len_exceeded(t *testing.T) {
+	s := store.New(5, 9)
+	err := s.Set("foo", "0123456789")
+	assert.ErrorIs(t, err, store.ErrorMaxValLen)
+}
 func TestSetAndGet_success(t *testing.T) {
-	s := store.New()
-	s.Set("foo", "bar")
+	s := store.New(10, 10)
+	_ = s.Set("foo", "bar")
 
 	got, err := s.Get("foo")
 	assert.Nil(t, err)
@@ -16,9 +27,9 @@ func TestSetAndGet_success(t *testing.T) {
 }
 
 func TestSetAndGet_overwrite_value(t *testing.T) {
-	s := store.New()
-	s.Set("foo", "bar")
-	s.Set("foo", "buzz")
+	s := store.New(10, 10)
+	_ = s.Set("foo", "bar")
+	_ = s.Set("foo", "buzz")
 
 	got, err := s.Get("foo")
 	assert.Nil(t, err)
@@ -26,7 +37,7 @@ func TestSetAndGet_overwrite_value(t *testing.T) {
 }
 
 func TestGet_nonexistent_key(t *testing.T) {
-	s := store.New()
+	s := store.New(10, 10)
 	_, err := s.Get("foo")
 	assert.ErrorIs(t, err, store.ErrNotFound)
 }
